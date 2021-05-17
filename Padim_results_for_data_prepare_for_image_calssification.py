13bc28e17078bf6ea68418fec4930a31dfffec7a
15 machine <155051843@qq.com>
2021-05-17 03:19:54 +0000

    add dataset prepare from padim for image classifictaion

---
+++ Padim_results_for_data_prepare_for_image_calssification.py
@@ -0,0 +1,53 @@
+import json
+import os,random,cv2
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy.core.arrayprint import printoptions
+
+def split(full_list,shuffle=False,ratio=0.2):
+    n_total = len(full_list)
+    offset = int(n_total * ratio)
+    if n_total==0 or offset<1:
+        return [],full_list
+    if shuffle:
+        random.shuffle(full_list)
+    sublist_1 = full_list[:offset]
+    sublist_2 = full_list[offset:]
+    
+    return sublist_1,sublist_2
+
+
+classes={"0": "lianjiao", "1": "shakong", "2": "guoshi", "3": "huashang", "4":"yanghua", "5":"tongheidian", "6":"heidian", "7":"feilinjiao", "8":"wuzi", "9":"yiwu"}
+json_path="/git/PaDiM-master/assets_new_new/data/2021-03-05/json_for_classification"
+image_name="Padim_results_image_label_for_classification.txt"
+save_folder="dataset_produce_from_padim"
+
+choose_label=[3,8,9]
+part_image_label_list=[]
+show_size=3
+num=1
+with open(os.path.join(json_path,image_name),"r") as fid:
+    image_label_list=fid.readlines()
+    for image_label in image_label_list:
+        image_name= image_label.split(" ")[0]
+        label= image_label.split(" ")[-1].strip()
+        if int(label) in choose_label:
+            part_image_label_list.append(image_label)
+
+
+data_train_list,data_list=split(part_image_label_list,ratio=0.7)
+data_val_list, data_test_list=split(data_list,ratio=0.333)
+print("train data num:{}, val data num:{}, test data num:{}".format(len(data_train_list),len(data_val_list),len(data_test_list)))
+
+
+save_data_list=[data_train_list,data_val_list,data_test_list]
+save_data_name=["train.txt","val.txt","test.txt"]
+for i in range(len(save_data_list)):
+    with open(os.path.join(json_path,save_folder,save_data_name[i]), 'w+',encoding="utf8") as fid:
+        for k in save_data_list[i]:
+            print(k)
+        fid.writelines([k  for k in save_data_list[i]])
+        print("{} is written".format(save_data_name[i]))
+  
+        
+            
