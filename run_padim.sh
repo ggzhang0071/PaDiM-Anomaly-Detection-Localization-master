d99962d5a05560124d04b96ca29e13b552971d70
15 machine <155051843@qq.com>
2021-05-19 03:40:54 +0000

    tmp

--- run_padim.sh
+++ run_padim.sh
@@ -11,7 +11,7 @@ do
   CUDA_VISIBLE_DEVICES=1,3  python3   main.py  --train_num_samples  0  --test_num_samples 0  --product_class $product_id --threshold_coefficient $threshold_coefficient --gaussian_blur_kernel_size 5 --save_path /git/PaDiM-master/kangqiang_result 2>&1 |tee Logs/$timestamp.log
 done
 
-
+<<COMMNET
 for product_id in  "0420QFN-5X6-8L" "0708DFN-8L"  "0713DFN-2X3-8L"  "1101QFN-40L" "1129QFN-4X4-24L" "DFN-5X6-8L" "DFN-5X6-T-8L" 
 do
   CUDA_VISIBLE_DEVICES=1,3  python3   main.py  --train_num_samples  0  --test_num_samples 0  --product_class $product_id --threshold_coefficient $threshold_coefficient --gaussian_blur_kernel_size 3 --save_path /git/PaDiM-master/kangqiang_result_3 2>&1 |tee Logs/$timestamp.log
@@ -27,3 +27,4 @@ done
 done
 
 python3 /git/PaDiM-master/assets_new_new/data/2021-03-05/json_for_classification/data_prepare_for_self-supervised_learning.py
+COMMENT
